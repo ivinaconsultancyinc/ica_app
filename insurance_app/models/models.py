9bb6a74ff8c195baaa32648772419c7d4fc3a2a4
ivinaconsultancyinc <ivinaconsultancyinc@gmail.com>
2025-09-26 14:06:39 +0000

    Update models.py

--- insurance_app/models/models.py
+++ insurance_app/models/models.py
@@ -4,6 +4,13 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    role = Column(String, nullable=False)
+
 class Client(Base):
     __tablename__ = "clients"
     id = Column(Integer, primary_key=True, index=True)
